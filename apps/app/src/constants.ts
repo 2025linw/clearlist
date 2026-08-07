5529a2eb97bb89d605f01e56ed031884dadac539
2025linw <2021linw39@gmail.com>
2026-08-07 08:38:52 -0500

    update url to new domain name

--- apps/app/src/constants.ts
+++ apps/app/src/constants.ts
@@ -7,7 +7,7 @@ const hostUri = Constants.expoConfig?.hostUri;
 const hostIp = hostUri?.split(':')[0];
 
 export const API_URL = !IS_DEV
-  ? 'https://todo.saphynet.io'
+  ? 'https://todo.saphydev.com'
   : Platform.OS === 'web'
     ? 'https://todo.localhost:8443'
     : `http://${hostIp}:8443`;
