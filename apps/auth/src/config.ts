f5ba37b5db12b142219595be9b2f556affcf9e29
2025linw <2021linw39@gmail.com>
2026-09-11 07:08:18 -0500

    set development url only in testing for auth trustedOrigins

--- apps/auth/src/config.ts
+++ apps/auth/src/config.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 
 export const trustedOrigins = [
-  'https://todo.localhost:8443',
   'https://todo.saphydev.com',
   'clearlist://',
+  ...(process.env.NODE_ENV === 'development'
+    ? ['https://todo.localhost:8443']
+    : []),
 ];
 
 export const port = process.env['SRV_PORT'];
