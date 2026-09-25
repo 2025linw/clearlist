9dc0c9995ac33734f3b6710162ad1b9505a3b4a2
2025linw <2021linw39@gmail.com>
2026-09-25 08:54:10 -0500

    [skip-ci] update workflows, make pull request only run on pulls to main, add way to skip ci and workflow

--- apps/app/src/constants.ts
+++ apps/app/src/constants.ts
@@ -1,5 +1,1 @@
-const IS_DEV = __DEV__;
-
-export const API_URL = !IS_DEV
-  ? 'https://todo.saphydev.com'
-  : 'https://will-laptop-kubuntu.tailae0279.ts.net:8080';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
