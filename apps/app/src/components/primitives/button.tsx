77638257e291de644f49d5a88ade7568f11440dc
2025linw <2021linw39@gmail.com>
2026-09-25 10:20:19 -0500

    remove hovered and return back to old format

--- apps/app/src/components/primitives/button.tsx
+++ apps/app/src/components/primitives/button.tsx
@@ -3,12 +3,10 @@ import {
   ColorValue,
   Pressable,
   PressableProps,
-  PressableStateCallbackType,
   StyleProp,
   StyleSheet,
   TextStyle,
   View,
-  ViewStyle,
 } from 'react-native';
 
 import { useTheme, useThemeMode } from '@contexts/theme';
@@ -68,24 +66,17 @@ export default function Button({
 
   const iconOnly = !!icon && !children;
 
-  function pressableStyleHandler(
-    state: PressableStateCallbackType,
-  ): StyleProp<ViewStyle> {
-    return [
-      styles.container,
-      props.rounded && styles.rounded,
-      iconOnly && styles.iconOnly,
-      'hovered' in state && state.hovered && styles.hoveredStyle,
-      state.pressed && styles.pressedStyle,
-      typeof style === 'function' ? style(state) : style,
-    ];
-  }
-
   return (
     <Pressable
       {...props}
       disabled={disabled}
-      style={pressableStyleHandler}
+      style={(state) => [
+        styles.container,
+        props.rounded && styles.rounded,
+        iconOnly && styles.iconOnly,
+        state.pressed && styles.pressedStyle,
+        typeof style === 'function' ? style(state) : style,
+      ]}
     >
       {icon}
 
